27dead4d8c9ca95921c463fa3745229be363dc71
sadafbatool <sadafbatool505@gmail.com>
2023-06-22 12:18:39 +0500

    added app cards to home page

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const appDetails = [
+  { title: "Calculator", href: "/calculator" },
+  { title: "CSS TODO", href: "/csstodo" },
+  { title: "MUI TODO", href: "/muitodo" },
+  { title: "COUNTER", href: "/tasbeeh" },
+];
